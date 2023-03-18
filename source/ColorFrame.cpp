7f9e595100d23ec9a9ebe8186595ca63bd0f06f7
OlesenJonas <45714731+OlesenJonas@users.noreply.github.com>
2023-03-18 21:27:06 +0100

    refactored further

--- source/ColorFrame.cpp
+++ source/ColorFrame.cpp
@@ -5,19 +5,29 @@
 #include "XEF/XEFEvent.hpp"
 
 // https://stackoverflow.com/questions/17892346/how-to-convert-rgb-yuv-rgb-both-ways#:~:text=VLFeat%2C%20or%20ImageMagick.-,2019%20Edit,-%3A%20Here%27s%20the
-void YUVtoRGBA(float Y, float U, float V, uint8_t* rgba)
+void YUVtoRGBA(uint8_t* YUYV, uint8_t* rgbaPair)
 {
-    Y -= 16.0f;
-    U -= 128.0f;
-    V -= 128.0f;
-    const float rf = 1.164f * Y + 1.596f * V;
-    const float gf = 1.164f * Y - 0.392f * U - 0.813f * V;
-    const float bf = 1.164f * Y + 2.017f * U;
+    float Y1 = YUYV[0] - 16.0f;
+    float U = YUYV[1] - 128.0f;
+    float Y2 = YUYV[2] - 16.0f;
+    float V = YUYV[3] - 128.0f;
 
-    rgba[0] = std::min(std::max(0.0f, rf), 255.0f);
-    rgba[1] = std::min(std::max(0.0f, gf), 255.0f);
-    rgba[2] = std::min(std::max(0.0f, bf), 255.0f);
-    rgba[3] = 255;
+    const float r1 = 1.164f * Y1 + 1.596f * V;
+    const float g1 = 1.164f * Y1 - 0.392f * U - 0.813f * V;
+    const float b1 = 1.164f * Y1 + 2.017f * U;
+
+    const float r2 = 1.164f * Y1 + 1.596f * V;
+    const float g2 = 1.164f * Y1 - 0.392f * U - 0.813f * V;
+    const float b2 = 1.164f * Y1 + 2.017f * U;
+
+    rgbaPair[0] = std::min(std::max(0.0f, r1), 255.0f);
+    rgbaPair[1] = std::min(std::max(0.0f, g1), 255.0f);
+    rgbaPair[2] = std::min(std::max(0.0f, b1), 255.0f);
+    rgbaPair[3] = 255;
+    rgbaPair[4] = std::min(std::max(0.0f, r2), 255.0f);
+    rgbaPair[5] = std::min(std::max(0.0f, g2), 255.0f);
+    rgbaPair[6] = std::min(std::max(0.0f, b2), 255.0f);
+    rgbaPair[7] = 255;
 };
 
 bool ColorFrame::replacePixelsWithNewUncompressedColorEvent(const XEFEvent& event)
@@ -40,8 +50,7 @@ bool ColorFrame::replacePixelsWithNewUncompressedColorEvent(const XEFEvent& even
         auto pixel1BaseIndex = (2 * i) * 4;
         auto pixel2BaseIndex = (2 * i + 1) * 4;
 
-        YUVtoRGBA(y1, u, v, &rgbPixels.data[pixel1BaseIndex]);
-        YUVtoRGBA(y2, u, v, &rgbPixels.data[pixel2BaseIndex]);
+        YUVtoRGBA((uint8_t*)&yuvPackets[i], &rgbPixels.data[pixel1BaseIndex]);
     }
     return true;
 }
